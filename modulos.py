12f1f86fac00e03e0f2081c28576b4da30efa7c9
Gonzalo <149430480+gcarpen2106@users.noreply.github.com>
2024-02-28 14:20:27 +0100

    Modulo InfoEquipos

---
+++ modulos.py
@@ -0,0 +1,45 @@
+import csv
+
+def InfoEquipos(datosliga, equipos):
+    info_equipos = []
+
+    for equipo in equipos:
+
+        ganados = 0
+        empatados = 0
+        perdidos = 0
+        puntos = 0
+
+        for partido in datosliga:
+
+            if partido['Team 1'] == equipo: #Comprobamos que el equipo 1 este dentro de los equipos
+
+                if partido['FT'][0] > partido['FT'][2]:   # Comprobamos que el equipo 1 ha ganado al equipo 2 para ello utilizamos la columna FT si la posicion 0 que los goles anotado por el 
+                                                          # equipo 1 es mayor a posicion 2 de la columna FT que son los goles del otro equipo pues realizamos la suma.
+                    ganados += 1
+                    puntos += 3
+
+                elif partido['FT'][0] == partido['FT'][2]:  # Comprobamos que el equipo 1 ha empatado al equipo 2 para ello utilizamos la columna FT si la posicion 0 que los goles anotado por el 
+                                                            # equipo 1 es igual a posicion 2 de la columna FT que son los goles del otro equipo pues realizamos la suma.
+                    empatados += 1
+                    puntos += 1
+                else:  # Si no es nada de esto pues a perdido el equipo 1
+                    perdidos += 1
+
+            elif partido['Team 2'] == equipo: #Comprobamos que el equipo 2 este dentro de los equipos
+                if partido['FT'][0] < partido['FT'][2]:  # Comprobamos que el equipo 2 ha ganado al equipo 1 para ello utilizamos la columna FT si la posicion 0 que los goles anotado por el 
+                                                         # equipo 2 es mayor a posicion 1 de la columna FT que son los goles del otro equipo pues realizamos la suma.
+                    ganados += 1
+                    puntos += 3
+
+                elif partido['FT'][0] == partido['FT'][2]:  # Comprobamos que el equipo 2 ha empatado al equipo 1 para ello utilizamos la columna FT si la posicion 0 que los goles anotado por el 
+                                                            # equipo 2 es igual a posicion 1 de la columna FT que son los goles del otro equipo pues realizamos la suma.
+                    empatados += 1
+                    puntos += 1
+                else:  # Si no es nada de esto pues a perdido el equipo 2
+                    perdidos += 1
+
+        info_equipos.append((equipo, ganados, empatados, perdidos, puntos))
+    return info_equipos
+
+
