34234880829f53d53d8eb27dac1f3af21d923309
pmuirod <158823491+pmuirod@users.noreply.github.com>
2024-02-29 13:24:45 +0100

    Update modulos.py

--- modulos.py
+++ modulos.py
@@ -1,14 +1,1 @@
-import csv
 
-def Puntos(info):
-    puntos = 0
-
-    for punto in info:
-        if punto == 1:
-            puntos += 3
-        elif punto == 0:
-            puntos += 1
-        else:
-            puntos += 0
-    
-    return puntos
