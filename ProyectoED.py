d38423ea99149edbfd5a0483dc8297aeb604ebd7
Gonzalo <149430480+gcarpen2106@users.noreply.github.com>
2024-02-29 16:34:14 +0100

    Update ProyectoED.py

--- ProyectoED.py
+++ ProyectoED.py
@@ -3,25 +3,11 @@ from modulos import LeerPartidos, impClasificacion, Equipos, InfoEquipos
 datosliga = 'liga.csv'
 
 
-from modulos import LeerPartidos, impClasificacion, Equipos, InfoEquipos
-
-datosliga = 'liga.csv'
-
-
-
-from modulos import LeerPartidos, impClasificacion, Equipos, InfoEquipos
-
-datosliga = 'liga.csv'
-
-
-
-
 liga = LeerPartidos()
 
 print("Partidos:")
 
 impClasificacion(liga)
-
 
 equipos = Equipos(datosliga)
 
@@ -34,43 +20,3 @@ info = InfoEquipos(liga,equipos)
 print("\n")
 
 print("Clasificación:",info)
-
-
-equipos = Equipos(datosliga)
-
-
-
-
-
-print("\n")
-
-
-print("Equipos:",equipos)
-
-
-info = InfoEquipos(liga,equipos)
-
-print("Partidos:")
-
-impClasificacion(liga)
-
-equipos = Equipos(datosliga)
-
-
-
-print("\n")
-
-print("Equipos:",equipos)
-
-
-info = InfoEquipos(liga,equipos)
-
-
-info = InfoEquipos(liga,equipos)
-
-from modulos import LeerPartidos, impClasificacion, Equipos, InfoEquipos
-
-datosliga = 'liga.csv'
-
-liga = LeerPartidos()
-
