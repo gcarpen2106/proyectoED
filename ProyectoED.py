c58447275eaeed6fb2b53dfb565e55ee806bc64c
pmuirod <158823491+pmuirod@users.noreply.github.com>
2024-02-29 14:42:29 +0100

    Creado clasificacion

---
+++ ProyectoED.py
@@ -0,0 +1,21 @@
+from modulos import LeerPartidos, impClasificacion, Equipos, InfoEquipos
+
+datosliga = 'liga.csv'
+
+liga = LeerPartidos()
+
+print("Partidos:")
+
+impClasificacion(liga)
+
+equipos = Equipos(datosliga)
+
+print("\n")
+
+print("Equipos:",equipos)
+
+info = InfoEquipos(liga,equipos)
+
+print("\n")
+
+print("Clasificación:",info)
