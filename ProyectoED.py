54b63f897081c38bc5a9beda716b23e44931d54f
pmuirod <158823491+pmuirod@users.noreply.github.com>
2024-02-29 13:26:19 +0100

    Update ProyectoED.py

--- ProyectoED.py
+++ ProyectoED.py
@@ -1,15 +1,1 @@
-from modulos import LeerPartidos, impClasificacion, Equipos, InfoEquipos
 
-datosliga = 'liga.csv'
-
-liga = LeerPartidos()
-
-impClasificacion(liga)
-
-equipos = Equipos(datosliga)
-
-info = InfoEquipos(liga,equipos)
-
-print("\n")
-
-print(equipos)
