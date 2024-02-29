28766e78472a045d275a95369d762a0510d3253f
Gonzalo <149430480+gcarpen2106@users.noreply.github.com>
2024-02-29 14:40:32 +0100

    Creado equipos

---
+++ ProyectoED.py
@@ -0,0 +1,15 @@
+from modulos import LeerPartidos, impClasificacion, Equipos, InfoEquipos
+
+datosliga = 'liga.csv'
+
+liga = LeerPartidos()
+
+print("Partidos:")
+
+impClasificacion(liga)
+
+equipos = Equipos(datosliga)
+
+print("\n")
+
+print("Equipos:",equipos)
---
+++ modulos.py
@@ -0,0 +1,24 @@
+import csv
+
+def LeerPartidos():
+    lista = []
+    with open ("liga.csv","r") as fichero:
+        contenido = csv.DictReader(fichero, delimiter=",")
+        next(contenido)
+
+        for i in contenido:
+            lista.append(i)
+    return lista
+
+def impClasificacion(liga):
+    for i in liga:
+        print(i)
+
+def Equipos(datosliga):
+    equipos = []
+    with open(datosliga, 'r') as f:
+        reader = csv.DictReader(f, delimiter=",")
+        for row in reader:
+            equipos.append(row['Team 1'])
+            
+    return set(equipos)
